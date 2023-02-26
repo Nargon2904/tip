a04d3c52eb4f82ad9f39dcb4dc469f28e66da4c3
AnthonyNargon2904 <antoga2004@yandex.ru>
2023-02-26 23:55:31 +0300

    scnd task

---
+++ 2.1/ConsoleApplication1.cpp
@@ -0,0 +1,52 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+class Triangle {
+private:
+    double a, b, c;
+public:
+    bool exst_tr() {
+        return ((a + b > c) && (b + c > a) && (a + c > b));
+    }
+    void set(double a1, double b1, double c1) {
+        a = a1;
+        b = b1;
+        c = c1;
+    }
+    void show() {
+        cout << "1 storona: " << a << endl;
+        cout << "2 storona: " <<b << endl;
+        cout << "3 storona: " << c << endl;
+    }
+    double perimetr() {
+        double p = a + b + c;
+        return p;
+    }
+    double square() {
+        double p = (a + b + c) / 2;
+        double s = sqrt(p * (p - a) * (p - b) * (p - c));
+        return s;
+    }
+};
+
+int main() {
+    Triangle mas[3];
+    double a, b, c;
+    for (int i = 0; i < 3; i++) {
+        cout << "vvedi" << i + 1 << endl;
+        cin >> a >> b >> c;
+        mas[i].set(a, b, c);
+        if (!(mas[i].exst_tr())) {
+            mas[i].show();
+            cout << "ne sush" << endl;
+            i--;
+        }
+    }
+    for (int i = 0; i < 3; i++) {
+        mas[i].show();
+        cout << "perimetr:" << mas[i].perimetr() << endl;
+        cout << "ploshad:" << mas[i].square() << endl;
+    }
+    return 0;
+}
---
+++ 2.2/ConsoleApplication2.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include "Header.h"
+#include <cmath>
+
+using namespace std;
+
+int main() {
+	//введите 0 0 1 \ 1 1 2.5 \ 1 1 1, чтобы проверить работу функций
+	setlocale(LC_ALL, "Rus");
+	float x, y, r;
+	circle mas[3];
+	for (int i = 0; i < 3; i++) {
+		cout << "Введите х, у и радиус " << i + 1 << "-й окружности: " << endl;
+		cin >> x >> y >> r;
+		mas[i].set_circle(r, x, y);
+	}
+	for (int i = 0; i < 3; i++) {
+		cout << "Площадь " << i + 1 << "-й окружности: " << endl;
+		cout << mas[i].square() << endl;
+		cout << "Можно ли вписать в " << i + 1 << "-ю окружность треугольник с длинами сторон 3, 4, 5?" << endl;
+		if (mas[i].triangle_in(3, 4, 5)) {
+			cout << "Да" << endl;
+		}
+		else {
+			cout << "Нет" << endl;
+		}
+		cout << "Можно ли описать около " << i + 1 << "-й окружности треугольник с длинами сторон 5, 12, 13?" << endl;
+		if (mas[i].triangle_around(5, 12, 13)) {
+			cout << "Да" << endl;
+		}
+		else {
+			cout << "Нет" << endl;
+		}
+		cout << "Пересекается ли " << i + 1 << "-я окружность с Circle (4, 1, 3)?" << endl;
+		if (mas[i].check_circle(4, 1, 3)) {
+			cout << "Да" << endl;
+		}
+		else {
+			cout << "Нет" << endl;
+		}
+	}
+}
